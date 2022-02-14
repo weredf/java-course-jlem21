1bf3599f35a254d0fa88758cb6740058bc92bd8f
weredf <mailwered@gmail.com>
2022-02-14 16:28:25 +0200

    14.2

--- 13-02-22-spring-for-ajax/src/main/java/app/core/controllers/ItemController.java
+++ 13-02-22-spring-for-ajax/src/main/java/app/core/controllers/ItemController.java
@@ -21,7 +21,7 @@ import app.core.services.ItemService;
 
 @RestController
 @RequestMapping("/items")
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+//@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class ItemController {
 
 	@Autowired
